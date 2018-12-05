7b8c2aeda65f38a27b58cb89b800484c279b4190
Dylan A <dylanpatrickavery@gmail.com>
2018-12-04 22:24:25 -0700

    Setup model, add logic to seed db on start of app

--- api/models.py
+++ api/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class CardManager(models.Manager):
+    def  create_card(self, id, name, age, team):
+        card = self.create(id=id,name=name, age=age, team=team)
+        return card 
+
 class Card(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=200)
     age = models.CharField(max_length=200)
     team = models.CharField(max_length=200)
-
-    def __init__(self, name, age, team):
-        self.name = name
-        self.age = age
-        self.team = team
-
+    
+    objects = CardManager()
     def __str__(self):
         """String for representing the Model object."""
         return self.name
@@ -26,6 +28,10 @@ class Card(models.Model):
     def getCards(self, cards):
         my_cards = list()
         for card in cards:
-            cardObject = self.objects.get(name=card)
-            my_cards.append(cardObject.name)
+            try:
+                cardObject = self.objects.get(name=card)
+                my_cards.append(cardObject.name)
+            except: 
+                cardObjects = self.objects.all()
+                # self.objects.all().delete()
         return my_cards
--- api/views.py
+++ api/views.py
@@ -6,12 +6,15 @@ from api.models import Card
 
 
 def index(request):
-
     season_totals = client.players_season_totals(season_end_year=2018)
     for index, player in enumerate(season_totals):
-        print(player)
-        c = Card(player['name'], player['age'], player['team'])
-        print(c.team.value.title())
+        try:
+            Card.objects.create_card(index, player['name'], player['age'], player['team'])
+        except:
+            pass
+    
+    allcards2 = Card.objects.all() 
+    print(len(allcards2))
     context = {
         'season_totals': season_totals,
     }
