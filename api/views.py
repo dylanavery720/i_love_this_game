cbcdb7f4abbd614b5ad7166d50e5e134ead3cf00
Dylan A <dylanpatrickavery@gmail.com>
2018-12-04 20:39:03 -0700

    Setup api and make api call

--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from basketball_reference_web_scraper import client
 # Create your views here.
 
 def index(request):
 
-    context = {
-        # 'num_songs': num_songs,
-    }
+    season_totals = client.players_season_totals(season_end_year=2018)
 
+    context = {
+        'season_totals': season_totals,
+    }
+    print(season_totals)
     return render(request, 'index.html', context=context)
