ff6ebf37c8206f9579f914e25d6bc3aac2a9f6c9
Dylan A <dylanpatrickavery@gmail.com>
2018-12-04 21:14:03 -0700

    Add logic to grab team from enum, capitalize first letter of each word in team using title method

--- api/models.py
+++ api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Card(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.CharField(max_length=200)
+    team = models.CharField(max_length=200)
+
+    def __init__(self, name, age, team):
+        self.name = name
+        self.age = age
+        self.team = team
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('card-detail', args=[str(self.id)])
+
+    def iter(self, cards):
+        my_iter = iter(cards)
+        return my_iter
+
+    def getCards(self, cards):
+        my_cards = list()
+        for card in cards:
+            cardObject = self.objects.get(name=card)
+            my_cards.append(cardObject.name)
+        return my_cards
--- api/views.py
+++ api/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from basketball_reference_web_scraper import client
 # Create your views here.
 
+from api.models import Card
+
+
 def index(request):
 
     season_totals = client.players_season_totals(season_end_year=2018)
-
+    for index, player in enumerate(season_totals):
+        print(player)
+        c = Card(player['name'], player['age'], player['team'])
+        print(c.team.value.title())
     context = {
         'season_totals': season_totals,
     }
-    print(season_totals)
     return render(request, 'index.html', context=context)
